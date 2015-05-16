b3b4ed3d1eef73c6b7f56ce09244cad4f1eeefb8
barestides <braden@arestides.com>
2015-05-16 13:52:20 -0400

    Made a method for scanning each file to make things a little more organized

--- playercounter.py
+++ playercounter.py
@@ -17,37 +17,54 @@ players = []
 
 start_time = time.time()
 
-# For storing each line in a file in a list
-lines = []
-
 count = 0
 
-for file in files:
 
+def scan_file(lines):
+
+    for line in lines:
+
+        file_name = os.path.basename(file)
+        date_stamp = file_name[:-7]
+
+        # regex to look for lines where player joins server
+        if re.search(r'^\[\d\d:\d\d:\d\d\] \[User Authenticator #\d{1,10}/INFO\]: UUID of player.*$', line):
+
+            time_stamp = line[:10]
+
+            # probably a better way to do this, but it scans through the line to find the location of the player name
+            player = line[line.find('UUID') + 15:line.find(' is ')]
+            if player not in players:  # if player isn't already in list
+                    players.append(player)
+                    hour_joined = time_stamp[1:3]
+                    popular_time[hour_joined] += 1
+                    # print(player.ljust(20), date_stamp, time_stamp)
+
+            # if re.search('\[Server thread/INFO\]$', line[11:31]):
+
+        if re.search(r'^\[\d\d:\d\d:\d\d\] \[Server thread/INFO\]:.*<.*>.*$', line):
+            time_stamp = line[:10]
+            player_name = line[line.find('INFO]: <') + 8 : line.find('>')]
+            message_content = line[line.find('>') + 2:]
+            output_file = open('chat-history.txt',mode='a',encoding='utf-8')
+            output_file.write(time_stamp)
+            output_file.write('\t')
+            output_file.write(player_name)
+            output_file.write(':')
+            output_file.write(message_content)
+            output_file.write('\n')
+            output_file.close()
+
+for file in files:
     file_size = os.path.getsize(file)
+    # For storing each line in a file in a list
+    lines = []
     if file_size < 500000:  # TODO change this to be less lazy, purpose is to ignore the huge files full of errors
         with gzip.open(file, 'rt', encoding='utf-8') as log_file:
             lines = list(log_file)
             count += len(lines)
 
-    for line in lines:
-
-        # regex to look for lines where player joins server
-        if re.search(r'^\[\d\d:\d\d:\d\d\] \[User Authenticator #\d{1,10}/INFO\]: UUID of player.*$', line):
-            # probably a better way to do this, but it scans through the line to find the location of the player name
-            player = line[line.find('UUID of player') + 15:line.find(' is ')]
-            if player not in players:  # if player isn't already in list
-                    players.append(player)
-                    file_name = os.path.basename(file)
-                    date_joined = file_name[:-7]
-                    time_joined = line[:10]
-                    hour_joined = time_joined[1:3]
-                    popular_time[hour_joined] += 1
-                    print(player.ljust(20), date_joined, time_joined)
-        # if re.search(r'^\[\d\d:\d\d:\d\d\] \[Server thread/INFO\]: <.*>.*$', line):
-        #     player_name = line[line.find('INFO]: <') + 8 : line.find('>')]
-        #     print(player_name)
+    scan_file(lines)
 
 end_time = time.time()
-print('Lines: ', count)
-
+print('Players: ', len(players), '\nTotal time: ', end_time-start_time)
